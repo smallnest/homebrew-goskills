cfcd5102b9517acb2a4d12255f4cc538e56eaecd
chaoyuepan <chaoyuepan@baidu.com>
2025-11-26 09:03:42 +0800

    update website

--- Formula/goskills.rb
+++ Formula/goskills.rb
@@ -8,7 +8,7 @@ class Goskills < Formula
     sha256 "f026aae03b42844754ab8b6211d3a2e606eceba9d12b94507e316217f688e8f4"  # 需要替换
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/smallnest/goskills/releases/download/v0.2.0/goskills_darwin_arm64.tar.gz"
-    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"  # 需要替换
+    sha256 "981cf0cfb666a9a4d84bd6ff1a89c416d95ea5d25dd7802ac9847de8707b1a46"  # 需要替换
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/smallnest/goskills/releases/download/v0.2.0/goskills_linux_amd64.tar.gz"
     sha256 "54936b2734b02637aacb4a36624db9101cb0fbe74df8c37effe6b4391def84db"  # 需要替换
