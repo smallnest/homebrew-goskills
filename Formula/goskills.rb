dee9df9ee8dd1fb2354587494498c8fcf3f1c0ed
smallnest <smallnest@gmail.com>
2026-01-08 20:59:54 +0800

    pump to v0.6.0

--- Formula/goskills.rb
+++ Formula/goskills.rb
@@ -1,17 +1,17 @@
 class Goskills < Formula
   desc "Go技能工具集"
   homepage "https://github.com/smallnest/goskills"
-  version "0.5.1"
+  version "0.6.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/smallnest/goskills/releases/download/v0.5.1/goskills_darwin_amd64.tar.gz"
-    sha256 "2d9b3a953c7c5f60db1b04739c0e9b346b08ce185cb4f32c58c85edc4d7b79a0"  # 需要替换
+    url "https://github.com/smallnest/goskills/releases/download/v0.6.0/goskills_darwin_amd64.tar.gz"
+    sha256 "cde290c5e7f041c3da8072f2fb082da5df791236b4859b0020f28af1f983cb58"  # 需要替换
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/smallnest/goskills/releases/download/v0.5.1/goskills_darwin_arm64.tar.gz"
-    sha256 "64a196c00fb6ed21b129625f1438cee392bb239c360cd2c189fd8266152ee0c9"  # 需要替换
+    url "https://github.com/smallnest/goskills/releases/download/v0.6.0/goskills_darwin_arm64.tar.gz"
+    sha256 "b58f4460d8309970c78a984d1c10878129271652d18c98ae80d23ea7a12dcb74"  # 需要替换
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/smallnest/goskills/releases/download/v0.5.1/goskills_linux_amd64.tar.gz"
-    sha256 "c35251483bce383efa79d76e21aa20cbe7f966342d822aac75a1fa5c3d5b06f7 "  # 需要替换
+    url "https://github.com/smallnest/goskills/releases/download/v0.6.0/goskills_linux_amd64.tar.gz"
+    sha256 "116aeb1af64bb2e830aca0b23e95d505b327a5ae9f844f70fee18f302b467e10 "  # 需要替换
   end
 
   def install
